8f5f90c861e5c620c851ab49b7921a7c6fdba268
Austin Kugler <aus343@gmail.com>
2019-01-03 23:25:24 -0600

    Finished Outline
    Finished basic outline for scouting app.

---
+++ BlitzScouter/Controllers/MainController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlitzScouter.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Scout(String[] teamInfo)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Scout()
+        {
+            return RedirectToAction("Data");
+        }
+
+        public IActionResult Data()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlitzScouter/Models/DataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlitzScouter.Models
+{
+    public class DataModel
+    {
+        public String teamNum { get; set; }
+        public String color { get; set; }
+    }
+}
