133f8f289121c1bd18d4451bb8038b3c591df8ff
Austin Kugler <aus343@gmail.com>
2019-01-26 00:19:15 -0600

    Exported to Excel File
    Added excel output to data.xlsx (added to .gitignore)

--- BlitzScouter/Controllers/MainController.cs
+++ BlitzScouter/Controllers/MainController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 
 namespace BlitzScouter.Controllers
 {
@@ -22,11 +24,45 @@ namespace BlitzScouter.Controllers
         [HttpPost]
         public IActionResult Data(Models.DataModel model)
         {
+            uploadData(model); // TODO: Multi Threading
             return View();
         }
 
         // Redirect to Index When Manually Connecting to Scout or Data
         public IActionResult Scout() { return RedirectToAction("Index"); }
         public IActionResult Data() { return RedirectToAction("Index"); }
+
+        // Data Management Methods
+        private bool uploadData(Models.DataModel model)
+        {
+            string path = "./data.xlsx";
+            FileInfo info = new FileInfo(path);
+            
+            using (ExcelPackage ep = new ExcelPackage(info))
+            {
+                ExcelWorksheet sheet;
+                if (ep.Workbook.Worksheets.ToArray().Length > 0)
+                {
+                    sheet = ep.Workbook.Worksheets["Raw Data"];
+                }
+                else
+                {
+                    sheet = ep.Workbook.Worksheets.Add("Raw Data");
+                    sheet.Cells["A1"].Value = "Raw Data:";
+                }
+                //ep.SaveAs(info);
+
+                //sheet.InsertRow(sheet.Dimension.End.Row, 1);
+                sheet.Cells[sheet.Dimension.End.Row + 1, 1].Value = "0";
+                string[] data = model.condenseData();
+                for (int i = 0; i < data.Length; i++)
+                {
+                   sheet.Cells[sheet.Dimension.End.Row, i + 1].Value = data[i];
+                }
+
+                ep.SaveAs(info);
+            }
+            return System.IO.File.Exists(path);
+        }
     }
 }
--- BlitzScouter/Models/DataModel.cs
+++ BlitzScouter/Models/DataModel.cs
@@ -8,70 +8,63 @@ using OfficeOpenXml;
 
 namespace BlitzScouter.Models
 {
-    public class DataModel
-    {
-        // Default Values
-        public DataModel()
-        {
-            counter1 = "0";
-            counter2 = "0";
-            counter3 = "0";
-            counter4 = "0";
-            counter5 = "0";
-        }
+	public class DataModel
+	{
+		// Default Values
+		public DataModel()
+		{
+			counter1 = "0";
+			counter2 = "0";
+			counter3 = "0";
+			counter4 = "0";
+			counter5 = "0";
+		}
+        
+        // Condense Functions into an Array
+		public string[] condenseData()
+		{
+			string[] arr = new string[11];
+			
+			arr[0] = teamNum;
+			arr[1] = color;
 
-        // Data Management Methods
-        public bool uploadData()
-        {
-            //ExcelPackage Ep = new ExcelPackage();
-            //ExcelWorksheet Sheet = Ep.Workbook.Worksheets.Add("Report");
-            return false;
-        }
+			arr[2] = checkbox1.ToString();
+			arr[3] = checkbox2.ToString();
+			arr[4] = checkbox3.ToString();
+			arr[5] = checkbox4.ToString();
 
-        private String[] condenseData()
-        {
-            string[] arr = new string[11];
-            
-            arr[0] = teamNum;
-            arr[1] = color;
+			arr[6] = counter1;
+			arr[7] = counter2;
+			arr[8] = counter3;
+			arr[9] = counter4;
+			arr[10] = counter5;
 
-            arr[2] = checkbox1.ToString();
-            arr[3] = checkbox2.ToString();
-            arr[4] = checkbox3.ToString();
-            arr[5] = checkbox4.ToString();
-
-            arr[6] = counter1;
-            arr[7] = counter2;
-            arr[8] = counter3;
-            arr[9] = counter4;
-            arr[10] = counter5;
-
-            return arr;
-        }
+			return arr;
+		}
 
         // Team Data
         public String teamNum { get; set; }
-        public String color { get; set; }
+		public String color { get; set; }
 
-        // Scouting Data
-        [DisplayName("Crossed Line")]
-        public bool checkbox1 { get; set; }
-        [DisplayName("Placed Piece")]
-        public bool checkbox2 { get; set; }
-        [DisplayName("Foaled")]
-        public bool checkbox3 { get; set; }
-        [DisplayName("Broke")]
-        public bool checkbox4 { get; set; }
+		// Scouting Data
+		[DisplayName("Crossed Line")]
+		public bool checkbox1 { get; set; }
+		[DisplayName("Placed Piece")]
+		public bool checkbox2 { get; set; }
+		[DisplayName("Foaled")]
+		public bool checkbox3 { get; set; }
+		[DisplayName("Broke")]
+		public bool checkbox4 { get; set; }
 
-        [DisplayName("Fuel in Rocket")]
-        public String counter1 { get; set; }
-        [DisplayName("Fuel in Cargo Ship")]
-        public String counter2 { get; set; }
-        [DisplayName("Hatch in Rocket")]
-        public String counter3 { get; set; }
-        [DisplayName("Hatch in Cargo Ship")]
-        public String counter4 { get; set; }
-        [DisplayName("Habitat Level")]
-        public String counter5 { get; set; }
-    }
+		[DisplayName("Fuel in Rocket")]
+		public String counter1 { get; set; }
+		[DisplayName("Fuel in Cargo Ship")]
+		public String counter2 { get; set; }
+		[DisplayName("Hatch in Rocket")]
+		public String counter3 { get; set; }
+		[DisplayName("Hatch in Cargo Ship")]
+		public String counter4 { get; set; }
+		[DisplayName("Habitat Level")]
+		public String counter5 { get; set; }
+	}
 }
